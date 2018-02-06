ddd5043f840c45b0ece3774d02a644c3abcf7ac5
Martin Kocsis <egykettoharo@gmail.com>
2018-02-06 15:59:07 +0100

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express     = require('express');
-var bodyParser  = require('body-parser');
+var express         = require('express');
+var bodyParser      = require('body-parser');
+var {ObjectID}    = require('mongodb');
 
 var {mongoose}  = require('./db/mongoose.js');
 var {Todo}      = require('./models/todo.js');
@@ -30,6 +31,29 @@ app.get('/todos', (req, res) => {
      }, (e) => {
          res.status(400).send(e);
      });
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        res.status(404).send('Given id is not valid');
+    }
+
+    Todo.findById(id).then((todo) => {
+            // Not found
+            if (!todo) {
+                res.status(404).send('Given id is not found!');
+            }
+
+            res.send({
+                todo
+            });
+        })
+        // Invalid ID
+        .catch((e) => {
+            res.status(400).send();
+        });
 });
 
 app.listen(3000, () => {
